28501c356be01a8e016aa2b36f3f7e042ccb65b4
Ranjan kumar Singha <ranjansingha.coding@gmail.com>
2024-06-01 18:07:37 +0530

    Commit

--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-Hero(Hero& temp){
-    //     cout<<"Custom Copy Constructor called."<<endl;
-    //     this->health = temp.health;
-    //     this->level = temp.level;
-    //     strcpy(this->name, temp.name);
-    // }
