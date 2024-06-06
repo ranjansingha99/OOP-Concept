bde6fe7280b88fb60988ee807fbb0a26beb61822
Ranjan kumar Singha <ranjansingha.coding@gmail.com>
2024-06-06 21:47:45 +0530

    Commit

--- 18_Abstraction.cpp
+++ 18_Abstraction.cpp
@@ -1,3 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 // Abstraction: the process of only showing the necessary details to the user and hiding the other details in the background.
 
 // Find difference between Encapsulaton VS Abstraction
+
+class implementAbstraction {
+private:
+	int a, b;
+
+public:
+	// method to set values of
+	// private members
+	void set(int x, int y)
+	{
+		a = x;
+		b = y;
+	}
+
+	void display()
+	{
+		cout << "a = " << a << endl;
+		cout << "b = " << b << endl;
+	}
+};
+
+int main()
+{
+	implementAbstraction obj;
+	obj.set(10, 20);
+	obj.display();
+	return 0;
+}
+
+/* You can see in the above program we are not allowed to access the variables a and b directly, however, one can call the function set() 
+to set the values in a and b and the function display() to display the values of a and b. */
